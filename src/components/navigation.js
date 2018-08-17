dd76c65312ed0a9c22102750d75c2e1bb525851a
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-17 20:37:00 +0530

    added food jokes component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,1 +1,33 @@
 // https://mock-the-week.herokuapp.com/api
+
+
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import '../App.css';
+
+class Nav extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/">Mock The Week</Link>
+        </div>
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Food Jokes</Link>
+          </li>
+          <li>
+           <Link to="/celebrity_jokes">Celebrity Jokes</Link>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li><button className="btn btn-info log">Log In</button></li>
+          <li><button className="btn btn-danger log">Log out </button></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
--- src/utils/fetch_api_data.js
+++ src/utils/fetch_api_data.js
@@ -2,14 +2,22 @@ import axios from 'axios';
 
 const BASE_URL = 'https://mock-the-week.herokuapp.com';
 
-function getFoodData() {
+function get_food_jokes_data() {
   const url = `${BASE_URL}/api/jokes/food`;
-  return axios.get(url).then(response => response.data);
+  return axios.get(url).then(response => response.data)
+  .catch( (err) =>{
+  	console.log('err',err);
+  	return err;
+  })
 }
 
 function getCelebrityData() {
   const url = `${BASE_URL}/api/jokes/celebrity`;
-  return axios.get(url).then(response => response.data);
+  return axios.get(url).then(response => response.data)
+  .catch( (err) =>{
+  	console.log('err',err);
+  	return err;
+  })
 }
 
 export {getFoodData, getCelebrityData};
