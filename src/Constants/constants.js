0568a4b37f131070a943f2b610b576d03e2bb448
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-18 18:22:44 +0530

    user_login component logic done

---
+++ src/Constants/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://mock-the-week.herokuapp.com/api';
--- src/components/user_register.js
+++ src/components/user_register.js
@@ -1,11 +1,50 @@
 
 
+
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './navigation';
+import axios from 'axios';
+import {BASE_URL} from '../Constants/constants';
 
 
 class Register extends Component{
+
+	constructor(props){
+		super(props);
+		this.register = this.register.bind(this);
+
+		this.state = {
+			email : '' ,
+			password : '' 
+		}
+	}
+
+	handle_email_change(event){
+		this.setState({
+		    email : event.target.value
+		})
+	}
+	handle_password_change(event){
+		this.setState({
+		    password : event.target.value
+		})
+	}
+
+	register(){
+
+		axios.post(BASE_URL + '/user/register', {
+		    email: this.state.email,
+		    password: this.state.password
+		  })
+		  .then(function (response) {
+		    console.log(response);
+		  })
+		  .catch(function (error) {
+		    console.log(error);
+		  });
+	}
+
 	render(){
 		return(
 	
@@ -16,13 +55,13 @@ class Register extends Component{
 
 			    <div className="form-group">
 			      <label htmlFor="email">Email:</label>
-			      <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" />
+			      <input type="email" className="form-control" value = {this.state.email} onChange = {(e) => {this.handle_email_change(e)}} />
 			    </div>
 			    <div className="form-group">
 			      <label htmlFor="pwd">Password:</label>
-			      <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd" />
+			      <input type="password" className="form-control" value = {this.state.password} onChange = {(e) => {this.handle_password_change(e)}}/>
 			    </div>
-			    <button type="submit" className="btn btn-default">Submit</button>
+			    <button type="submit" className="btn btn-default" onClick = {this.register}>Submit</button>
 
 			</div>
 		</div>
